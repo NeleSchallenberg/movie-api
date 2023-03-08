d59001fb390afa8ce65a49da682e97d2eb5d332f
Regina Phalangy <nschallenberg@gmx.net>
2023-03-08 13:31:18 +0100

    Created topMovies variable and GET requests

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 // Importing Express module
 const express = require('express');
 const app = express();
+
+// Declaring variable for top 10 movies
+let topMovies = [
+    { 
+        title: 'Drive My Car',
+        year: '2021',
+        director: 'Ryusuke Hamaguchi',
+    },
+
+    { 
+        title: 'The Royal Tennenbaums',
+        year: '2001',
+        director: 'Wes Anderson',
+    },
+
+    { 
+        title: 'The Virgin Suicides',
+        year: '1999',
+        director: 'Sofia Coppola',
+    },
+
+    { 
+        title: 'Billy Elliot',
+        year: '2000',
+        director: 'Stephen Daldry',
+    },
+]
+
+// Creating GET requests
+app.get('/', (req, res) => {
+    res.send('Welcome to my movie club!')
+})
+app.get('/movies', (req, res) => {
+    res.json(topMovies)
+})
