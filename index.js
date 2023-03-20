66d9fafbfd5561a639fb06769bb9d9996eb1a91c
Regina Phalangy <nschallenberg@gmx.net>
2023-03-20 17:16:31 +0100

    Imported Mongoose and models and created requests

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+//Importing Mongoose package and models.js file
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+const Movie = Models.Movie;
+const User = Models.User;
+
+// Connecting to database "cfDB"
+mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
 // Importing Express module, Morgan logging middleware, body-parser and uuid
 const express = require('express'),
     morgan = require('morgan'),
@@ -10,163 +20,162 @@ app.use(express.static('public'));
 app.use(morgan('common'));
 app.use(bodyParser.json());
 
-// Declaring JSON objects for movies
-let movies = [
-    { 
-        'Title': 'Drive My Car',
-        'Description': '...',
-        'Genre': {
-            'Name': 'Drama',
-            'Description': 'In film and television, drama is a category or genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.'
-        },
-        'Year': 2021,
-        'Director': {
-            'Name': 'Ryusuke Hamaguchi',
-            'Bio': '...'
-        }
-    },
 
-    { 
-        'Title': 'The Royal Tennenbaums',
-        'Description': '...',
-        'Genre': {
-            'Name': '...',
-            'Description': '...'
-        },
-        'Year': 2001,
-        'Director': {
-            'Name': 'Wes Anderson',
-            'Bio': 'Wesley Wales Anderson was born on May 1, 1969, in Houston, Texas. His films are known for their eccentricity and unique visual and narrative styles. They often contain themes of grief, loss of innocence, and dysfunctional families.'
-        }
-    },
+// let movies = [
+//     { 
+//         'Title': 'Drive My Car',
+//         'Description': '...',
+//         'Genre': {
+//             'Name': 'Drama',
+//             'Description': 'In film and television, drama is a category or genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.'
+//         },
+//         'Year': 2021,
+//         'Director': {
+//             'Name': 'Ryusuke Hamaguchi',
+//             'Bio': '...'
+//         }
+//     },
 
-    { 
-        'Title': 'The Virgin Suicides',
-        'Description': '...',
-        'Genre': {
-            'Name': '...',
-            'Description': '...'
-        },
-        'Year': 1999,
-        'Director': {
-            'Name': 'Sofia Coppola',
-            'Bio': '...'
-        }
-    },
+//     { 
+//         'Title': 'The Royal Tennenbaums',
+//         'Description': '...',
+//         'Genre': {
+//             'Name': '...',
+//             'Description': '...'
+//         },
+//         'Year': 2001,
+//         'Director': {
+//             'Name': 'Wes Anderson',
+//             'Bio': 'Wesley Wales Anderson was born on May 1, 1969, in Houston, Texas. His films are known for their eccentricity and unique visual and narrative styles. They often contain themes of grief, loss of innocence, and dysfunctional families.'
+//         }
+//     },
 
-    { 
-        'Title': 'Billy Elliot',
-        'Description': '...',
-        'Genre': {
-            'Name': '...',
-            'Description': '...'
-        },
-        'Year': 2000,
-        'Director': {
-            'Name': 'Stephen Daldry',
-            'Bio': '...'
-        }
-    },
+//     { 
+//         'Title': 'The Virgin Suicides',
+//         'Description': '...',
+//         'Genre': {
+//             'Name': '...',
+//             'Description': '...'
+//         },
+//         'Year': 1999,
+//         'Director': {
+//             'Name': 'Sofia Coppola',
+//             'Bio': '...'
+//         }
+//     },
 
-    { 
-        'Title': 'The Life Aquatic with Steve Zissou',
-        'Description': '...',
-        'Genre': {
-            'Name': '...',
-            'Description': '...'
-        },
-        'Year': 2004,
-        'Director': {
-            'Name': 'Wes Anderson',
-            'Bio': '...'
-        }
-    },
+//     { 
+//         'Title': 'Billy Elliot',
+//         'Description': '...',
+//         'Genre': {
+//             'Name': '...',
+//             'Description': '...'
+//         },
+//         'Year': 2000,
+//         'Director': {
+//             'Name': 'Stephen Daldry',
+//             'Bio': '...'
+//         }
+//     },
 
-    { 
-        'Title': 'Inception',
-        'Description': '...',
-        'Genre': {
-            'Name': '...',
-            'Description': '...'
-        },
-        'Year': 2010,
-        'Director': {
-            'Name': 'Christopher Nolan',
-            'Bio': '...'
-        }
-    },
+//     { 
+//         'Title': 'The Life Aquatic with Steve Zissou',
+//         'Description': '...',
+//         'Genre': {
+//             'Name': '...',
+//             'Description': '...'
+//         },
+//         'Year': 2004,
+//         'Director': {
+//             'Name': 'Wes Anderson',
+//             'Bio': '...'
+//         }
+//     },
 
-    { 
-        'Title': 'La La Land',
-        'Description': '...',
-        'Genre': {
-            'Name': '...',
-            'Description': '...'
-        },
-        'Year': 2016,
-        'Director': {
-            'Name': 'Damien Chazelle',
-            'Bio': '...'
-        }
-    },
+//     { 
+//         'Title': 'Inception',
+//         'Description': '...',
+//         'Genre': {
+//             'Name': '...',
+//             'Description': '...'
+//         },
+//         'Year': 2010,
+//         'Director': {
+//             'Name': 'Christopher Nolan',
+//             'Bio': '...'
+//         }
+//     },
 
-    { 
-        'Title': 'Mystic River',
-        'Description': '...',
-        'Genre': {
-            'Name': '...',
-            'Description': '...'
-        },
-        'Year': 2003,
-        'Director': {
-            'Name': 'Clint Eastwood',
-            'Bio': '...'
-        }
-    },
+//     { 
+//         'Title': 'La La Land',
+//         'Description': '...',
+//         'Genre': {
+//             'Name': '...',
+//             'Description': '...'
+//         },
+//         'Year': 2016,
+//         'Director': {
+//             'Name': 'Damien Chazelle',
+//             'Bio': '...'
+//         }
+//     },
 
-    { 
-        'Title': 'Across the Universe',
-        'Description': '...',
-        'Genre': {
-            'Name': '...',
-            'Description': '...'
-        },
-        'Year': 2007,
-        'Director': {
-            'Name': 'Julie Taymor',
-            'Bio': '...'
-        }
-    },
+//     { 
+//         'Title': 'Mystic River',
+//         'Description': '...',
+//         'Genre': {
+//             'Name': '...',
+//             'Description': '...'
+//         },
+//         'Year': 2003,
+//         'Director': {
+//             'Name': 'Clint Eastwood',
+//             'Bio': '...'
+//         }
+//     },
 
-    { 
-        'Title': 'Cold War',
-        'Description': '...',
-        'Genre': {
-            'Name': '...',
-            'Description': '...'
-        },
-        'Year': 2018,
-        'Director': {
-            'Name': 'Paweł Pawlikowski',
-            'Bio': '...'
-        }
-    },
-]
+//     { 
+//         'Title': 'Across the Universe',
+//         'Description': '...',
+//         'Genre': {
+//             'Name': '...',
+//             'Description': '...'
+//         },
+//         'Year': 2007,
+//         'Director': {
+//             'Name': 'Julie Taymor',
+//             'Bio': '...'
+//         }
+//     },
 
-// Declaring JSON objects for users
-let users = [
-    {
-        id: 1,
-        name: 'Regina',
-        favoriteMovies: ['The Prestige']
-    },
+//     { 
+//         'Title': 'Cold War',
+//         'Description': '...',
+//         'Genre': {
+//             'Name': '...',
+//             'Description': '...'
+//         },
+//         'Year': 2018,
+//         'Director': {
+//             'Name': 'Paweł Pawlikowski',
+//             'Bio': '...'
+//         }
+//     },
+// ]
 
-    {
-        id: 2,
-        name: 'Miles',
-        favoriteMovies: ['Pulp Fiction']
-    }
-]
+// let users = [
+//     {
+//         id: 1,
+//         name: 'Regina',
+//         favoriteMovies: ['The Prestige']
+//     },
+
+//     {
+//         id: 2,
+//         name: 'Miles',
+//         favoriteMovies: ['Pulp Fiction']
+//     }
+// ]
 
 // Returns a welcome message
 app.get('/', (req, res) => {
@@ -211,18 +220,45 @@ app.get('/movies/directors/:directorName', (req, res) => {
     }
 });
 
-// Registers new user
+// POST request creating a new user, expecting JSON format
 app.post('/users', (req, res) => {
-    const newUser = req.body;
-
-    if (newUser.name) {
-        newUser.id = uuid.v4();
-        users.push(newUser);
-        res.status(201).json(newUser)
-    } else {
-        res.status(400).send('Name is required.')
-    }
+    Users.findOne({Username: req.body.Username}).then((user) => {
+        if (user) {
+            return res.status(400).send(req.body.Username + 'already exists.')
+        } else {
+            Users.create({
+                Username: req.body.Username,
+                Password: req.body.Password,
+                Email: req.body.Email,
+                Birthday: req.body.Birthday
+            })
+            .then((user) => {
+                res.status(201).json(user);
+            })
+            .catch((error) => {
+                console.error(error);
+                res.status(500).send('Error:' + error);
+            })
+        }
+    })
+    .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error:' + error);
+    });
 });
+
+// // Registers new user
+// app.post('/users', (req, res) => {
+//     const newUser = req.body;
+
+//     if (newUser.name) {
+//         newUser.id = uuid.v4();
+//         users.push(newUser);
+//         res.status(201).json(newUser)
+//     } else {
+//         res.status(400).send('Name is required.')
+//     }
+// });
 
 // Updates user name
 app.put('/users/:id', (req, res) => {
