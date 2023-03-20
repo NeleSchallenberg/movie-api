16e47ab823941b64386a720574afa617e36026bc
Regina Phalangy <nschallenberg@gmx.net>
2023-03-20 17:25:30 +0100

    Update models.js

--- models.js
+++ models.js
@@ -22,7 +22,6 @@ let movieSchema = mongoose.Schema({
 
 // Defining schema for users
 let userSchema = mongoose.Schema({
-    ID: {type: String, required: true},
     Username: {type: String, required: true},
     Password: {type: String, required: true},
     Email: {type: String, required: true},
