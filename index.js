6e320291fe10ae0951df6430afd7c824d0175328
Regina Phalangy <nschallenberg@gmx.net>
2023-03-21 09:45:49 +0100

    Updated PUT request

--- index.js
+++ index.js
@@ -97,25 +97,26 @@ app.post('/users', (req, res) => {
 
 // PUT request updating user data by name, expecting JSON format
 app.put('/users/:Username', (req, res) => {
-  Users.findOneAndUpdate({Username: req.params.Username}, 
-    {$set: 
-      {
-        Username: req.body.Username,
-        Password: req.body.Password,
-        Email: req.body.Email,
-        Birthday: req.body.Birthday
-      }
-    }, 
-    {new: true},
-    (err, updatedUser) => {
-      if(err) {
-        console.error(err);
-        res.status(500).send('Error: ' + err);
-      } else {
-        res.json(updatedUser);
-      }
-    });
+  Users.findOneAndUpdate({Username: req.params.Username}, {$set:
+    {
+      Username: req.body.Username,
+      Password: req.body.Password,
+      Email: req.body.Email,
+      Birthday: req.body.Birthday
+    }
+  },
+  {new: true},
+  (err, updatedUser) => {
+    if(err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
 });
+
+
 
 // POST request adding movie to user favorite list by ID
 app.post('/users/:Username/movies/:movieID', (req, res) => {
