831e0495a42c85b4121cf542baccbec7e48f109f
Regina Phalangy <nschallenberg@gmx.net>
2023-04-26 18:56:31 +0200

    Revert "Update CORS"
    This reverts commit 3f71fed6279d55f9daa5166eea5fece6963978cb.

--- index.js
+++ index.js
@@ -23,20 +23,8 @@ app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Cross-Origin Recource Sharing policy
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://female-filmmakers.netlify.app'];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 let auth = require('./auth')(app);
 
